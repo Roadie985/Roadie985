ceb098be9d16794ad04669f460ec714ddc97957a
Roadie <63084255+Roadie985@users.noreply.github.com>
2021-05-19 11:36:05 +0100

    Add files via upload

---
+++ BaconSandwich/BaconSandwich/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BaconSandwich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+                Console.WriteLine("Do you want a bacon sandwich?");
+                Console.Write("Yes or no? ");
+                var ans1 = Console.ReadLine();
+                var ansshow = ans1.ToString();
+
+
+                Console.WriteLine(ansshow +" WAS YOUR GIVEN ANSWER");
+                if (ans1 == "YES")
+                {
+                    
+                    Console.WriteLine("Good choice");
+                   
+                }
+            if (ans1 == "NO")
+            {
+
+                Console.WriteLine("Oh yes you do");
+
+            }
+
+        }
+          
+        
+    }
+}
